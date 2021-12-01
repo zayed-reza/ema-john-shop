13393b0a293092975825d4d629f9fd2a0ce941d0
zayed-reza <zayedreza500@gmail.com>
2021-12-01 09:47:17 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCFn4wNifKq8xko6ao1eMl7GtTh3CxzJmk",
+    authDomain: "ema-jhon-shop-a80c9.firebaseapp.com",
+    projectId: "ema-jhon-shop-a80c9",
+    storageBucket: "ema-jhon-shop-a80c9.appspot.com",
+    messagingSenderId: "774536202956",
+    appId: "1:774536202956:web:bca0ffbab17913d9c91069"
+};
+
+export default firebaseConfig;
